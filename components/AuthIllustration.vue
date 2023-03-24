f7c8e8e6a2031d179a4c852e2c87bfb0d728ba39
armin <arminsb91@gmail.com>
2023-03-24 15:23:43 +0330

    auth pages(register) started

---
+++ components/AuthIllustration.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-textWhite hidden md:flex justify-center items-center flex-col bg-gradient-to-b from-authGradientFrom to-authGradientTo w-6/12 h-screen">
+    <img class="max-w-[577px]" src="@/assets/images/illustrations/auth.svg" alt="auth-vector" />
+    <h1 class="text-2xl px-[75px] pb-2">لورم ایپسوم متن ساختگی با تولید سادگی</h1>
+    <p class="text-center px-4">
+      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ لورم ایپسوم متن
+      ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "authGradientFrom":"#6264A7",
+        "authGradientTo":"#5438DC",
+        "darkBg":"#191C2F",
+        "textWhite":"#FAFAFA",
+        "primaryBlue":"#5438DC",
+      }
+    },
+  },
+  plugins: [],
+}
